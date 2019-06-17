73cc26b3a5967232bb8f7d9ba4e72ad7532c82f0
171258 <Lyudmila@pn1617235.lanl.gov>
2019-06-17 11:47:34 -0600

    autogen: update site

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -1,101 +1,21 @@
----
-# Content index for Tipue Search
-# https://github.com/jekylltools/jekyll-tipue-search
-# v1.4
-layout: null
----
-{%- assign index = "" | split: "" -%}
-{%- assign excluded_files = site.tipue_search.exclude.files -%}
-{%- assign excluded_tags = site.tipue_search.exclude.tags | uniq -%}
-{%- assign excluded_categories = site.tipue_search.exclude.categories | uniq -%}
-{%- assign excluded_taxonomies = excluded_tags | concat: excluded_categories | uniq -%}
-{%- for post in site.posts -%}
-  {%- unless post.exclude_from_search == true or excluded_files contains post.path -%}
-    {%- assign has_excluded_taxonomy = false -%}
-    {%- for tag in post.tags -%}
-      {%- if excluded_taxonomies contains tag -%}
-        {%- assign has_excluded_taxonomy = true -%}
-      {%- endif -%}
-    {%- endfor -%}
-    {%- for category in post.categories -%}
-      {%- if excluded_taxonomies contains category -%}
-        {%- assign has_excluded_taxonomy = true -%}
-      {%- endif -%}
-    {%- endfor -%}
-    {%- unless has_excluded_taxonomy == true -%}
-      {%- assign index = index | push: post | uniq -%}
-    {%- endunless -%}
-  {%- endunless -%}
-{%- endfor -%}
-{%- for page in site.traces -%}
-{%- unless page.exclude_from_search == true or excluded_files contains page.path -%}
-{%- assign has_excluded_taxonomy = false -%}
-{%- for tag in page.tags -%}
-{%- if excluded_taxonomies contains tag -%}
-{%- assign has_excluded_taxonomy = true -%}
-{%- endif -%}
-{%- endfor -%}
-{%- for category in page.categories -%}
-{%- if excluded_taxonomies contains category -%}
-{%- assign has_excluded_taxonomy = true -%}
-{%- endif -%}
-{%- endfor -%}
-{%- unless has_excluded_taxonomy == true -%}
-{%- assign index = index | push: page | uniq -%}
-{%- endunless -%}
-{%- endunless -%}
-{%- endfor -%}
-{%- if site.tipue_search.include.pages == true -%}
-  {%- for page in site.html_pages -%}
-    {%- unless page.exclude_from_search == true or excluded_files contains page.path -%}
-      {%- assign has_excluded_taxonomy = false -%}
-      {%- for tag in page.tags -%}
-        {%- if excluded_taxonomies contains tag -%}
-          {%- assign has_excluded_taxonomy = true -%}
-        {%- endif -%}
-      {%- endfor -%}
-      {%- for category in page.categories -%}
-        {%- if excluded_taxonomies contains category -%}
-          {%- assign has_excluded_taxonomy = true -%}
-        {%- endif -%}
-      {%- endfor -%}
-      {%- unless has_excluded_taxonomy == true -%}
-        {%- assign index = index | push: page | uniq -%}
-      {%- endunless -%}
-    {%- endunless -%}
-  {%- endfor -%}
-{%- endif -%}
-{%- for collection in site.tipue_search.include.collections -%}
-  {%- assign documents = site.documents | where:"collection",collection -%}
-  {%- for document in documents -%}
-    {%- unless document.exclude_from_search == true or excluded_files contains document.path -%}
-      {%- assign has_excluded_taxonomy = false -%}
-      {%- for tag in document.tags -%}
-        {%- if excluded_taxonomies contains tag -%}
-          {%- assign has_excluded_taxonomy = true -%}
-        {%- endif -%}
-      {%- endfor -%}
-      {%- for category in document.categories -%}
-        {%- if excluded_taxonomies contains category -%}
-          {%- assign has_excluded_taxonomy = true -%}
-        {%- endif -%}
-      {%- endfor -%}
-      {%- unless has_excluded_taxonomy == true -%}
-        {%- assign index = index | push: document | uniq -%}
-      {%- endunless -%}
-    {%- endunless -%}
-  {%- endfor -%}
-{%- endfor -%}
-var tipuesearch = {"pages": [
-{%- for document in index -%}
-  {%- assign tags = document.tags | uniq -%}
-  {%- assign categories = document.categories | uniq -%}
-  {%- assign taxonomies = tags | concat: categories | uniq -%}
-  {
-    "title": {{ document.title | smartify | strip_html | normalize_whitespace | jsonify }},
-    "text": {{ document.content | strip_html | normalize_whitespace | jsonify }},
-    "tags": {{ taxonomies | join: " " | normalize_whitespace | jsonify }},
-    "url": {{ document.url | relative_url | jsonify }}
-  }{%- unless forloop.last -%},{%- endunless -%}
-{%- endfor -%}
-]};
+var tipuesearch = {"pages": [{
+    "title": "Welcome to Jekyll!",
+    "text": "You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated. To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works. Jekyll also offers powerful support for code snippets: def print_hi(name) puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.",
+    "tags": "jekyll update",
+    "url": "/jekyll/update/2019/06/13/welcome-to-jekyll.html"
+  },{
+    "title": "",
+    "text": "404 Page not found :( The requested page could not be found.",
+    "tags": "",
+    "url": "/404.html"
+  },{
+    "title": "About",
+    "text": "This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll / jekyll",
+    "tags": "",
+    "url": "/about/"
+  },{
+    "title": "Traces",
+    "text": "{% directory path: traces %} {{ file.name }}{% unless forloop.last %}, {% endunless %} {% enddirectory %} You can find the source code for Jekyll at GitHub: [jekyll][jekyll-organization] / [jekyll](https://github.com/jekyll/jekyll) [jekyll-organization]: https://github.com/jekyll",
+    "tags": "",
+    "url": "/traces/"
+  }]};
